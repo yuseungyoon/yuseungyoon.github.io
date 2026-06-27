d54e31933db935893c572407d9d0dddcb03cdcbb
y7oon <seung.yoon.yu@gmail.com>
2026-06-27 18:46:50 +0900

    fix: 작은 뷰포트에서 타이틀 단어를 글자 단위로 자르도록 수정

--- features/post/components/PostTitle.css.ts
+++ features/post/components/PostTitle.css.ts
@@ -1,9 +1,10 @@
 import { style } from '@vanilla-extract/css'
 import { dp } from 'styles/dp'
+import { breakpoints } from 'styles/vars/breakpoints.css'
 import { color } from 'styles/vars/color.css'
 
 export const title = style({
-  fontSize: dp(18),
+  fontSize: dp(12),
   marginBottom: dp(4),
   color: color.text,
   textAlign: 'left',
@@ -11,4 +12,11 @@ export const title = style({
   fontWeight: 700,
   fontFamily: '"Mona Sans", "Pretendard", monospace',
   lineHeight: 1.15,
+  wordBreak: 'break-all',
+  '@media': {
+    [breakpoints.desktop]: {
+      fontSize: dp(18),
+      wordBreak: 'break-word',
+    },
+  },
 })
